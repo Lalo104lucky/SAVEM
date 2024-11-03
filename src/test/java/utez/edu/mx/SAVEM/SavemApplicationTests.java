25b0390e6c2797a6861c14a9b6cbe4fbe0b76833
ael3023 <20223TN023@utez.edu.mx>
2024-11-02 21:16:59 -0600

    subido:D

---
+++ src/test/java/utez/edu/mx/SAVEM/SavemApplicationTests.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.SAVEM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
